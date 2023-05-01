6f166620b0c40b2eb6b6d3026f5ae51b0de34ccb
Akshay <akshayrdhotre@gmail.com>
2023-05-01 13:25:52 +0530

    Ex4 completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArr) {
+    const args = Array.prototype.slice.call(arguments,1);
+    for(let i = 0; i < args.length; i++)
+    {
+        
+        let index = myArr.findIndex(function(x){
+            return x===args[i];
+        });
+        if(index >= 0)
+        {
+            myArr.splice(index,1);
+        }
+        if(typeof(args[i]) === typeof("s"))
+        {
+            continue;
+        } 
+        
+    }
+    console.log(myArr);
+    return myArr;
 };
 
 // Do not edit below this line
