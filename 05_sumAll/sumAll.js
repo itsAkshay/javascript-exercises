83acdad2321903b731ac0c07ba9681591c4b5523
Akshay <akshayrdhotre@gmail.com>
2023-05-01 13:58:34 +0530

    Ex5 & Ex6 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
 
+    if(start < 0 || end < 0||typeof(start) !== typeof(1)||typeof(end) !== typeof(1))
+    {
+        return "ERROR";
+    }
+
+    let sum = 0;
+    n = end-start;
+    if(n < 0)
+    {
+        n *= -1;
+    }
+    console.log("n:"+n);
+    sum = ((n+1) * (start+end))/2;
+    console.log(sum);
+    return sum;
 };
 
 // Do not edit below this line
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,21 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //divisble by 4
+    //not divisible by 100
+    //divisible by 100 & 400
+    if(year % 4 === 0)
+    {
+        if(year % 100 === 0)
+        {
+            if(year % 400 === 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    return false;
 
 };
 
